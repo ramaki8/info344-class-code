a25786af080c1d959edcd2a3dcf68a3d1b6e22d4
Rachel Kipps <rachelkipps@gmail.com>
2016-01-06 10:19:55 -0800

    End of class time.

---
+++ php/warmup.php
@@ -0,0 +1,16 @@
+
+<?php
+    // Simple Warm-Up
+    $rand = rand(1, 100);
+    echo 'Your new random value is ' . $rand . ".\n";
+    
+    //Arrays and Loops
+    date_default_timezone_set("America/New_York");
+    $months = array();
+    for($i = 1; $i <  13; $i++) {
+        $month = date("F", mktime(0,0,0,$i,1,2016));
+        array_push($months, $month);
+    }
+    foreach($months as $m) {
+        echo $m . ' ';
+    }
