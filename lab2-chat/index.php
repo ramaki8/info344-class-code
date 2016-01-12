18536b606a7d2abd1b28d64acd7bd698b73628a7
Rachel Kipps <rachelkipps@gmail.com>
2016-01-11 21:12:02 -0800

    end of lab 1/11

---
+++ lab2-chat/index.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once 'connection.php';
+
+$q = $_GET['q'];
+
+$conn = getConnection();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta charset="UTF-8">
+    <link rel="icon" href="img/page-icon.png">
+    <title>Chat</title>
+    
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+</head>
+<body class="container">
+    <?php 
+    include 'views/search-form.php';
+    
+    include 'views/matches.php';
+    
+    if (isset($weatherData)) {
+        include 'views/weather.php';
+    }
+    ?>
+   
+</body>
+</html>
